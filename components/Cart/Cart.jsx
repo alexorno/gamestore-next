63a10d196a280522b297510f760e76164a72f312
Alex <audut1197@gmail.com>
2023-04-27 16:04:16 +0200

    added Cart animation

--- components/Cart/Cart.jsx
+++ components/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useStateContext } from '../../context/StateContext';
 import CartItem from './CartItem';
 import { loadStripe } from '@stripe/stripe-js'
@@ -8,8 +8,23 @@ import Stripe from 'stripe';
 
 
 const Cart = () => {
-  
+  const cartContainer = useRef()
   const {cartItems, useCartItems, setShowCart, totalPrice} = useStateContext()
+  
+ useEffect(() => {
+    cartContainer.current.classList.add('animate')
+   setTimeout(() => {
+    cartContainer.current.classList.remove('animate')
+   }, 10);
+ }, [])
+
+ const closeCart = () =>{
+  cartContainer.current.classList.add('animate')
+  setTimeout(() => {
+    setShowCart(false)
+  }, 1500);
+ }
+ 
   
   const handleCheckout = async () => {
       const stripePromise = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
@@ -27,15 +42,15 @@ const Cart = () => {
       const data = await response.json()
 
 
-      // toast.loading('loaddddd')
+      toast.loading('Loading...')
 
       stripePromise.redirectToCheckout({sessionId: data.id})
   }
 
   return (
 
-    <div className='cart-container'>
-      <button className='close-icon' onClick={() => {setShowCart(false)}}>
+    <div className='cart-container' ref={cartContainer}>
+      <button className='close-icon' onClick={() => {closeCart()}}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-12 h-12">
           <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
         </svg>
